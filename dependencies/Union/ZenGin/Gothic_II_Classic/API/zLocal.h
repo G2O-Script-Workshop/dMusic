7eae7c18cbfd00392d0cf4ff8d393806b4eeee1d
Patrix <patrixon9999@wp.pl>
2023-03-18 04:32:47 +0100

    Updated Union/GothicAPI:
    - Added fix that prevent default classes constructors being called !n times (caused by default cpp behaviour, which calls the default base constructor by default)
    - Adjusted project to API changes/updates
    - Added dx7 & dx8 SDK (unfortunately this is required by the Union, but it's not that bad, might be useful in the future)
    - Removed inclusion of headers/sources for other Gothic versions (G1, Sequel, G2 (without NoTR))

--- dependencies/Union/ZenGin/Gothic_II_Classic/API/zLocal.h
+++
@@ -1,7 +0,0 @@
-// Supported with union (c) 2018-2021 Union team
-
-#ifndef __ZLOCAL_H__VER2__
-#define __ZLOCAL_H__VER2__
-
-
-#endif // __ZLOCAL_H__VER2__
--- dependencies/Union/ZenGin/Gothic_I_Classic/API/oMenu_Help.h
+++
@@ -1,7 +0,0 @@
-// Supported with union (c) 2018-2021 Union team
-
-#ifndef __OMENU__HELP_H__VER0__
-#define __OMENU__HELP_H__VER0__
-
-
-#endif // __OMENU__HELP_H__VER0__

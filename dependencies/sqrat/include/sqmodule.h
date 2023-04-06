edf4703ef9c73b8d4ebf647a71d17726a41156d9
Patrix <patrixon9999@wp.pl>
2023-04-07 00:02:37 +0200

    Updated sqrat dependency to 3.2 squirrel version

--- dependencies/sqrat/include/sqmodule.h
+++ dependencies/sqrat/include/sqmodule.h
@@ -113,10 +113,10 @@ extern "C" {
         SQRESULT        (*gettypetag)(HSQUIRRELVM v,SQInteger idx,SQUserPointer *typetag);
         void            (*setreleasehook)(HSQUIRRELVM v,SQInteger idx,SQRELEASEHOOK hook);
         SQChar*         (*getscratchpad)(HSQUIRRELVM v,SQInteger minsize);
-        SQRESULT        (*getclosureinfo)(HSQUIRRELVM v,SQInteger idx,SQUnsignedInteger *nparams,SQUnsignedInteger *nfreevars);
+        SQRESULT        (*getclosureinfo)(HSQUIRRELVM v,SQInteger idx,SQInteger *nparams,SQInteger *nfreevars);
         SQRESULT        (*setnativeclosurename)(HSQUIRRELVM v,SQInteger idx,const SQChar *name);
         SQRESULT        (*setinstanceup)(HSQUIRRELVM v, SQInteger idx, SQUserPointer p);
-        SQRESULT        (*getinstanceup)(HSQUIRRELVM v, SQInteger idx, SQUserPointer *p,SQUserPointer typetag);
+        SQRESULT        (*getinstanceup)(HSQUIRRELVM v, SQInteger idx, SQUserPointer *p,SQUserPointer typetag, SQBool throwerror);
         SQRESULT        (*setclassudsize)(HSQUIRRELVM v, SQInteger idx, SQInteger udsize);
         SQRESULT        (*newclass)(HSQUIRRELVM v,SQBool hasbase);
         SQRESULT        (*createinstance)(HSQUIRRELVM v,SQInteger idx);

0bbf4bbfab4ebf74e5314afc50b150194565df13
Patrix <patrixon9999@wp.pl>
2024-03-09 01:52:49 +0100

    Replaced CMakeSettings.json with CMakePresets.json

--- windows-x86.cmake
+++
@@ -1,9 +0,0 @@
-# Simple toolchain file for compiling under windows
-# Usage: cmake .. -DCMAKE_TOOLCHAIN_FILE=windows-x86.cmake
-
-# specify the target system properties
-set(CMAKE_SYSTEM_NAME Windows)
-
-if (CMAKE_GENERATOR MATCHES "Visual Studio*")
-	set(CMAKE_GENERATOR_PLATFORM Win32 CACHE INTERNAL "")
-endif()

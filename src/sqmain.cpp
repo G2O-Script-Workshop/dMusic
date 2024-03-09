4deda19aa1dba0d442c137f9292a7a98217b78c6
Patrix <patrixon9999@wp.pl>
2024-03-09 02:52:45 +0100

    Added minimalistic hooking example

--- src/sqmain.cpp
+++ src/sqmain.cpp
@@ -1,4 +1,31 @@
 #include <sqapi.h>
+#include <Union/Hook.h>
+#include <ZenGin/zGothicAPI.h>
+
+using namespace Gothic_II_Addon;
+
+/* squirreldoc (func)
+*
+* This is an entry point for the module.
+*
+* @version	0.1
+* @side		client
+* @name		oCGame__Render
+* @param	(oCGame*) self the oCGame ptr to object instance.
+* @param	(void*) vtable the vftable param.
+*
+*/
+void __fastcall oCGame__Render(oCGame* self, void* vtable);
+auto Hook_oCGame_Render = Union::CreateHook((void*)0x006C86A0, oCGame__Render, Union::HookType::Hook_Detours);
+void __fastcall oCGame__Render(oCGame* self, void* vtable)
+{
+	printf("render call\n");
+	Hook_oCGame_Render(self, vtable);
+
+	// Since g2o hook is overriding Union master hook, we need to reapply it
+	Hook_oCGame_Render.Disable();
+	Hook_oCGame_Render.Enable();
+}
 
 extern "C" SQRESULT SQRAT_API sqmodule_load(HSQUIRRELVM vm, HSQAPI api)
 {

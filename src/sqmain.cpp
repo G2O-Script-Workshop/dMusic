0b4f1099e8d29b5c5c0879299b7bed592e8846f8
Arkhan <49446701+DamianQualshy@users.noreply.github.com>
2024-12-23 14:44:20 +0100

    Update sqmain.cpp
    fixed typo, comment unnecessary include

--- src/sqmain.cpp
+++ src/sqmain.cpp
@@ -1,11 +1,13 @@
-#if __G2A && SCART_EXPORT
+#if __G2A && SCRAT_EXPORT
 
-#include "sqUtils.h"
+// #include "sqUtils.h"
 #include "sqFunction.h"
 
 extern "C" SQRESULT SQRAT_API sqmodule_load(HSQUIRRELVM vm, HSQAPI api)
 {
 	SqModule::Initialize(vm, api);
+	Sqrat::DefaultVM::Set(vm);
+
 
 	Sqrat::RootTable(vm)
 		.Func("playTheme", &sq_playTheme)
